9f9e6530c076ffe6c29eb53ac6d9b36d66e36ec6
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-07 23:00:09 -0500

    Work on turnkey.sh

--- turnkey.sh
+++ turnkey.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 
-# Stop installation
+# Insert commands to install Command Line Tools
+
+# Pause installation
 # Tell user to downloads apps from App Store
-# Reminder about XCode
-# Tips n shit to start
 
-read -p "Press [Enter] when everything is downloads."
+echo "Download all your apps from the Apple App Store."
+echo "Don't worry, I'll wait ;)"
+read -p "Press [Enter] when everything is downloaded."
 
-# Install homebrew
+# Install Homebrew
 
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+
+# Update Homebrew
+
+brew update
 
 # Check homebrew for problems
 
 brew doctor
 
 # Install all software
-cd scripts
-brew bundle Brewfile
-
+brew bundle scripts/Brewfile
