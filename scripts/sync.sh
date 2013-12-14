5caf9431ecb2b3b798bd04b4a37259aedefea233
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-14 12:53:41 -0500

    Add .z folder to excludes

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 function doIt() {
         rsync --exclude ".git/" --exclude ".DS_Store" --exclude "scripts/" --exclude "turnkey.sh" --exclude "._*" \
-                --exclude "README.md" --exclude "Brewfile" --exclude "LICENSE-MIT.txt" -av --no-perms ../ ~
+                --exclude "README.md" --exclude ".z/" --exclude "Brewfile" --exclude "LICENSE-MIT.txt" -av --no-perms ../ ~
         source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
