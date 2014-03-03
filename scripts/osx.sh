392ce493c30b5a25b31ad5334de0bc0b5cbaf234
mAAdhaTTah <jamesorodig@gmail.com>
2014-03-02 20:16:38 -0500

    Add hotcorners, Fix #4

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -176,6 +176,28 @@ defaults write com.apple.dock minimize-to-application -bool true
 # Show indicator lights for open applications in the Dock
 defaults write com.apple.dock show-process-indicators -bool true
 
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 10
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Bottom right screen corner → Desktop
+defaults write com.apple.dock wvous-br-corner -int 4
+defaults write com.apple.dock wvous-br-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 2
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
 read -p "Do you want to wipe all the apps in the dock? Only do this on a new install. (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
